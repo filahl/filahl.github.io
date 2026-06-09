c4e08ad369cf36646789ed6baf6144ce7d7c6933
filahl <83027380+filahl@users.noreply.github.com>
2026-06-09 10:04:09 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'hub-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=JetBrains+Mono:wght@400&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Vrátí z cache
+        }
+        return fetch(event.request);
+      })
+  );
+});
